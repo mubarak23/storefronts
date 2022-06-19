2bcbc90c6d3ade523d11a0e5da697d80b88e73c6
mubarak23 <mubarakaminu340@gmail.com>
2022-06-19 01:21:42 +0100

    categories setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Anounce from '../components/Anounce'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
+
 function Home() {
   return (
     <div>
       <Anounce />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   )
 }
